0e5af37d84a8bb8d8a30f27a0d316edb4ca03278
Carla Valéria <carla_empresa@outlook.com>
2017-11-27 17:50:56 -0300

    ajeitando o pequisar do apenas listar component

--- src/app/apenas-listar/apenas-listar.component.ts
+++ src/app/apenas-listar/apenas-listar.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
@@ -9,12 +10,18 @@ export class ApenasListarComponent implements OnInit {
 
   @Input("metodoListar")  metodoListar : any = {};
   
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
     
   }
-
+  pesquisar(s:string ): void{
+    this.http.get('http://localhost:8080/?nome=' + s)
+    .subscribe(data => {
+      this.metodoListar = data;
+      console.log(data);
+    });
+  }
   metodoAddListar(curso){
     this.metodoListar.push(curso);
     //return this.ngOnInit();
